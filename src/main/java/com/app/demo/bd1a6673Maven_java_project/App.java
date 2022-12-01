e07fa0ee3337125dc05020f1fddda51b7420b2d4
josephlavanya <josephlavanya68@gmail.com>
2022-12-01 10:08:47 +0530

    Maven_java_project

---
+++ src/main/java/com/app/demo/bd1a6673Maven_java_project/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.bd1a6673Maven_java_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
